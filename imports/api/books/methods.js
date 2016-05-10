59d39c81b0769918ffa732fdf7099401ad7bb051
Stephen Printup <stephen.printup@gmail.com>
2016-05-10 16:09:19 -0600

    updates

--- imports/api/books/methods.js
+++ imports/api/books/methods.js
@@ -2,7 +2,6 @@ import './books';
 
 Meteor.methods({
   'Books.insert' ( book ) {
-    console.log("what's happening book: "+book);
     Books.insert( book, ( error ) => {
       if ( error ) {
         console.log( error );
